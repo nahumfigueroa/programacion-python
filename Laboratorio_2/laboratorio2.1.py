4b51963f4411c0aa03a99adfc4b78057c61b2ca3
nahumfigueroa <ramironahumfigueroacastro@gmail.com>
2016-12-08 20:59:50 -0600

    Add files via upload

---
+++ Laboratorio_2/laboratorio2.1.py
@@ -0,0 +1,7 @@
+#Ramiro Nahum Figueroa Castro
+#25/11/2016
+import math
+
+x = float(input("lado 1:"))
+y = float(input("lado 2:"))
+print "Hipotenusa igual a: " +str(math.sqrt((x*x)+(y*y)))
---
+++ Laboratorio_2/laboratorio2.2.py
@@ -0,0 +1,5 @@
+banderazo=8.74
+def distancia(km):
+    n=(km/0.25)*1.84
+    dinero=n+banderazo
+    print dinero, "pesos"
